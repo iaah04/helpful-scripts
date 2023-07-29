99745bef79755717d6f0583b9166f6845281dc54
iaah04 <blablablaaaa777@gmail.com>
2023-07-30 00:07:44 +0200

    first commit

---
+++ password_generator/generatepassword_copy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import argparse
+import random
+import string
+
+parser = argparse.ArgumentParser(description="Generate a password.")
+parser.add_argument("-l", "--length", type=int, help="Length of password.")
+args = parser.parse_args()
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + "!@#$%^&*()"
+    password = "".join(random.choice(characters) for _ in range(length))
+    return password
+
+if args.length:
+    if args.length < 12:
+        print("Password length cannot be less than 12.")
+    elif args.length > 20:
+        print("Password length cannot exceed 20.")
+    else:
+        password = generate_password(args.length)
+        print(password)
+else:
+    password = generate_password(12)
+    print(password)
